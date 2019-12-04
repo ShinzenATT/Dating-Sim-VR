c851bb85bc374ba7e9116a689b25e027ce62865a
ShinzenATT <42674003+ShinzenATT@users.noreply.github.com>
2019-12-04 11:53:37 +0100

    Adjusted Player Camera and added abillity to show hand with controllers

---
+++ Assets/Scripts/ShowController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ShowController : MonoBehaviour
+{
+    public bool ShowControllers = false;
+    bool ShowControllersState = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ControllerVisibillity();
+
+    }
+
+    void ControllerVisibillity()
+    {
+        foreach (var hand in Player.instance.hands)
+        {
+            if (ShowControllers && !ShowControllersState)
+            {
+                hand.ShowController();
+            }
+            else if (!ShowControllers && ShowControllersState)
+            {
+                hand.HideController();
+            }
+        }
+
+        if (ShowControllers != ShowControllersState)
+        {
+            ShowControllersState = ShowControllers;
+        }
+    }
+}
