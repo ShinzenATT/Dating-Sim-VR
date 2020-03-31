636809d10acb4c0df30d6ed95b05483624141098
Alen.M <42674003+ShinzenATT@users.noreply.github.com>
2020-03-31 20:29:34 +0200

    Attempt at loading dialog from a text file (not working)

---
+++ Assets/Scripts/DialogTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DialogTree : MonoBehaviour
+{
+    public static List<string[]> DialogTrees { get; } = new List<string[]> 
+    { 
+        File.ReadAllLines("..\\Dialog\\test.txt") 
+    };
+}
